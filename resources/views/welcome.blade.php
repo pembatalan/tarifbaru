41973e21c31fd234e59824f019d331781f699f01
Alysson Melo <alyssoncm@gmail.com>
2023-05-10 16:23:55 -0300

    initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Welcome to Back4app Contianers</title>
+
+        <!-- CSS -->
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="header-text-shadow">Welcome to Back4app Contianers</h1>
+        </div>
+    </body>
+</html>
