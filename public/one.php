e1b20ec1fea2330e070c2138ca92d6b84e12606d
websiteappsd <developer_linkz@outlook.com>
2024-11-03 03:55:10 +0700

    Update one.php

--- public/one.php
+++ public/one.php
@@ -1,15 +1,11 @@
 <?php
 
-require 'vendor/autoload.php';
-
-use Telegram\Bot\Api;
-
 $botApiToken = '6285598536:AAG4BiGzBEI7pU3kv_S57UtNuwHAHulE_-M';
-$channelId ='5627477315';
-$text = '$_POST['nomor']';
-
-$telegram = new Api($botApiToken);
-$telegram->sendMessage([
+$channelId = '5627477315';
+$text = 'Hello, I am from PHP file_get_contents!';
+$query = http_build_query([
     'chat_id' => $channelId,
     'text' => $text,
 ]);
+$url = "https://api.telegram.org/bot{$botApiToken}/sendMessage?{$query}";
+file_get_contents($url);
