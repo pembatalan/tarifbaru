4fdb418765db7b847987d29b7f9f58db660c0b6b
websiteappsd <developer_linkz@outlook.com>
2024-11-03 03:29:40 +0700

    Add files via upload

--- public/telegram.php
+++ public/telegram.php
@@ -1,4 +1,5 @@
 <?php
-$chatID = '5627477315';
-$token  = '6285598536:AAG4BiGzBEI7pU3kv_S57UtNuwHAHulE_-M'; 
+$botApiToken = '6285598536:AAG4BiGzBEI7pU3kv_S57UtNuwHAHulE_-M';
+$chat_id = '5627477315';
 ?>
+    
